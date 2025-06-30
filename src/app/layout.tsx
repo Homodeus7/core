48831267e81d643ca4f8ca54c8dff791323fd149
Vyacheslav Androsov <vyacheslavandrosov@MacBook-Pro-Vyacheslav.local>
2025-06-30 12:57:56 +0700

    chore: added prettier and shadsc/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/shared/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const fontSans = FontSans({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}`}
       >
         {children}
       </body>
