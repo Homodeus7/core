d6989b4bb8701ad658835bd774e7b5dfd4d01ad5
plumbus <plum.bus@yahoo.com>
2025-07-09 14:41:34 +0700

    chore: added prisma and docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { PrismaClient } from "@/generated/prisma";
 import { Button } from "@/shared/components/ui/button";
 
-export default function Home() {
+const client = new PrismaClient();
+
+export default async function Home() {
+  const courses = await client.course.findMany();
+
+  console.log({ courses });
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <Button>Button</Button>
